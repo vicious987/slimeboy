b7cc9daa91242470dba1736d31f22b8e45e84b55
vicious <vicious987@gmail.com>
2021-08-24 12:08:25 +0200

    add pause menu

--- MainMenu.gd
+++ MainMenu.gd
@@ -6,13 +6,13 @@ func _ready() -> void:
 	$VBoxContainer/StartButton.connect("pressed", self, "start_game")
 	$VBoxContainer/OptionsButton.connect("pressed", self, "open_options")
 	$VBoxContainer/ExitButton.connect("pressed", self, "quit_game")
+	print("menu loaded!")
 
 func quit_game():
 	get_tree().quit()
 
 func open_options():
-	pass
+	print("options clicked!")
 	
 func start_game():
 	get_tree().change_scene("res://Game.tscn")
-	#emit_signal("start_level", "res://levels/level_1.tscn")
---
+++ Popup.gd
@@ -0,0 +1,28 @@
+extends Popup
+
+func _input(event: InputEvent) -> void:
+	if event.is_action_pressed("ui_cancel"):
+		if get_tree().paused:
+			unpause_game()
+		else:
+			pause_game()
+
+func _ready() -> void:
+	$VBoxContainer/ResumeButton.connect("pressed", self, "unpause_game")
+	$VBoxContainer/OptionsButton.connect("pressed", self, "open_options")
+	$VBoxContainer/QuitButton.connect("pressed", self, "quit_to_menu")
+	
+func unpause_game() -> void:
+	get_tree().paused = false
+	hide()
+
+func pause_game() -> void:
+	get_tree().paused = true
+	show()
+
+func open_options():
+	pass
+
+func quit_to_menu():
+	unpause_game()
+	get_tree().change_scene("res://MainMenu.tscn")
